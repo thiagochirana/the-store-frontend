8dc2d6221274f45310753e9d91df02f0879000a9
Chirana <engsw.thiago@gmail.com>
2025-01-11 02:45:41 -0300

    feat: adjust main layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,12 @@
-// src/components/Layout.jsx
 import { Header } from "./header/Header";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className="flex">
-      <div className="flex-1">
+    <div className="flex h-screen bg-gray-50">
+      <div className="flex-1 flex flex-col">
         <Header />
-        <div className="pt-8">
+        <div className="pt-1 flex-1 w-full bg-gray-50">
           <Outlet />
         </div>
       </div>
